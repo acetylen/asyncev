fcbb193c82661589ede3f6779c91f8d530c0bc80
Emil Tylén <emil@tylen.se>
2025-07-20 23:23:09 +0200

    fix typing

--- asyncev/__init__.py
+++ asyncev/__init__.py
@@ -1,23 +1,23 @@
 from functools import wraps
 from typing import Any, Callable
 
-from .asyncev import AsyncEv, BaseEvent as Event, Listener
+from .asyncev import AsyncEv, BaseEvent as Event, Listener, EventType
 
 _default = AsyncEv()
 
 
 @wraps(_default.bind)
-def bind(event: type[Event], func: Listener):
+def bind(event: type[EventType], func: Listener[EventType]) -> None:
     _default.bind(event, func)
 
 
 @wraps(_default.unbind)
-def unbind(event: type[Event], func: Listener):
+def unbind(event: type[EventType], func: Listener[EventType]) -> None:
     _default.unbind(event, func)
 
 
 @wraps(_default.emit)
-def emit(event: Event):
+def emit(event: Event) -> None:
     _default.emit(event)
 
 
@@ -27,12 +27,12 @@ async def gather(event: Event) -> list[Any]:
 
 
 @wraps(_default.gather_for)
-def gather_for(event: Event, func: Callable[..., None]):
+def gather_for(event: Event, func: Callable[[list[Any]], None]) -> None:
     _default.gather_for(event, func)
 
 
 @wraps(_default.wait_for)
-async def wait_for(event: type[Event]) -> Event:
+async def wait_for(event: type[EventType]) -> EventType:
     return await _default.wait_for(event)
 
 
--- asyncev/asyncev.py
+++ asyncev/asyncev.py
@@ -20,20 +20,23 @@ log = logging.getLogger(__name__)
 
 @dataclass
 class BaseEvent(ABC):
-    def __post_init__(self):
+    """Base class for event types."""
+
+    def __init__(self, *args, **kwargs):
         if type(self) is BaseEvent:
             raise Exception("BaseEvent should not be instantiated directly")
+        super().__init__(*args, **kwargs)
 
 
 EventType = TypeVar("EventType", bound=BaseEvent)
-Listener = Callable[[BaseEvent], Coroutine[None, None, Any]]
-Reference = Union[ReferenceType[Listener], WeakMethod[Listener]]
+Listener = Callable[[EventType], Coroutine[None, None, Any]]
+Reference = Union[ReferenceType[Listener[EventType]], WeakMethod[Listener[EventType]]]
 
 
 def funcref(
-    listener: Listener,
-    callback: Optional[Callable[[Reference], None]] = None,
-) -> Reference:
+    listener: Listener[EventType],
+    callback: Optional[Callable[[Reference[EventType]], None]] = None,
+) -> Reference[EventType]:
     """Unified way to create a reference to a function or a bound method."""
     if ismethod(listener):
         return WeakMethod(listener, callback)
@@ -47,10 +50,10 @@ class AsyncEv:
     """
 
     def __init__(self):
-        self.events: dict[type[BaseEvent], set[Reference]] = {}
+        self.events: dict[type[BaseEvent], set[Reference[BaseEvent]]] = {}
         self.writelock: dict[type[BaseEvent], asyncio.Lock] = {}
 
-    def bind(self, event: type[BaseEvent], listener: Listener):
+    def bind(self, event: type[EventType], listener: Listener[EventType]) -> None:
         """Bind {listener} to a named {event}."""
         if not iscoroutinefunction(listener):
             raise TypeError("Event listeners must be async!")
@@ -64,24 +67,24 @@ class AsyncEv:
             funcref(listener, lambda r: self.events[event].discard(r))
         )
 
-    def unbind(self, event: type[BaseEvent], listener: Listener):
+    def unbind(self, event: type[EventType], listener: Listener[EventType]) -> None:
         """Unbind {listener} from {event}.
         Will error if event or listener doesn't exist."""
         log.debug("Removing binding: %r -> %s", event, listener)
 
         self.events[event].discard(funcref(listener))
 
-    async def _emit(self, event: BaseEvent):
+    async def _emit(self, event: BaseEvent) -> list[Any]:
         """Call all valid {event} handlers with the provided events."""
         t = type(event)
         async with self.writelock[t]:
-            # the weakref callback _should_ ensure that no Nones are left in the list.
-            callbacks = [task()(event) for task in self.events[t]] # type: ignore
+            # the weakref callback ensures that no Nones are left in the list.
+            callbacks = [task()(event) for task in self.events[t]]
 
         log.debug("Emit %s!", t.__name__)
         return await asyncio.gather(*callbacks)
 
-    def emit(self, event: BaseEvent):
+    def emit(self, event: BaseEvent) -> None:
         """Emit {event}, triggering all listeners as soon as possible."""
         log.debug("Scheduling emit(%s)", event)
         asyncio.create_task(self._emit(event))
@@ -91,20 +94,20 @@ class AsyncEv:
         log.debug("Gather(%s)", event)
         return await self._emit(event)
 
-    def gather_for(self, event: BaseEvent, func: Callable[..., None]):
+    def gather_for(self, event: BaseEvent, func: Callable[[list[Any]], None]) -> None:
         """Emit {event}, wait for all listeners, and send their results to {func}.
 
         This is useful for passing results to synchronous functions.
         """
 
-        async def _callback():
+        async def callback() -> None:
             results = await self.gather(event)
             func(results)
 
         log.debug("Scheduling gather_for(%s)", event)
-        asyncio.create_task(_callback())
+        asyncio.create_task(callback())
 
-    async def wait_for(self, event: type[BaseEvent]) -> BaseEvent:
+    async def wait_for(self, event: type[EventType]) -> EventType:
         """Sleep until an {event} occurs, then return it.
 
         wait_for creates a temporary handler for the given event that gets
@@ -112,7 +115,7 @@ class AsyncEv:
         than registering a proper event handler, so should probably only be
         used for events that don't occur very often.
         """
-        future = asyncio.Future()
+        future: asyncio.Future[EventType] = asyncio.Future()
 
         async def wait_trigger(event: EventType) -> None:
             future.set_result(event)
